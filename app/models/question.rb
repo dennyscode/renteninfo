ae567079a0792f9ecbd72d76c12d3d5b55df6a30
Denny Sander <sander.bewerbung@gmx.de>
2019-11-17 22:03:43 +0100

    deleting erb + nested attributes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,2 @@
 class Question < ApplicationRecord
-    accepts_nested_attributes_for :rente_jobs, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
-    accepts_nested_attributes_for :rente_kinder_ges, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
-
 end
