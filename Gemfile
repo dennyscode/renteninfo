210a14f833e604ace27136459941e153aa5917ed
Denny Sander <sander.bewerbung@gmx.de>
2020-01-25 23:38:49 +0100

    Setup Sendgrid Mailer

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "figaro"
 gem 'rack', '~> 2.1', '>= 2.1.1'
 # Use Puma as the app server
 gem 'puma', '~> 3.4'
+gem 'sendgrid-ruby'
 # Use SCSS for stylesheets
 # gem 'sass-rails', '~> 5'
 # gem 'trix-rails', require: 'trix'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "renteninfo_production"
 
+ # Setup the mailer config
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.perform_deliveries = true
+ config.action_mailer.smtp_settings = {
+   :user_name => ENV['SENDGRID_USERNAME'],
+   :password => ENV['SENDGRID_PASSWORD'],
+   :domain => 'yourdomain.com',
+   :address => 'smtp.sendgrid.net',
+   :port => 587,
+   :authentication => :plain,
+   :enable_starttls_auto => true
+ }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
